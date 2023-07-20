83304b08cc7e9b2d447a9ca010093deb59292314
Shirsho-Lab <bsse1106@iit.du.ac.bd>
2023-07-20 20:23:33 +0600

    srs text posted to mongodb

---
+++ mongodb.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MongoDB:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            connection_url = os.environ.get("MONGO_CONNECTION_URL")
+            cls._instance.client = MongoClient(connection_url)
+        return cls._instance
+
+    def get_client(self):
+        return self.client
--- server.py
+++ server.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from mongodb import MongoDB
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_name = os.environ.get("DATABASE_NAME")
+srs_main_ep = os.environ.get("POST_SRS_MAIN_ENDPOINT")
+srs_main_collection = os.environ.get("SRS_MAIN_COLLECTION")
 
 app = FastAPI()
 
+mongodb = MongoDB()
+srs_database = mongodb.get_client()[database_name]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class SRSData(BaseModel):
+    text: str
+
+@app.post(srs_main_ep, status_code=201)
+async def add_srs_text(srs_data: SRSData):
+    text = srs_data.text
+    if not text:
+        raise HTTPException(status_code=400, detail="error in text srs adding", headers={"X-Error": "Text cannot be empty."})
+
+    # Insert the SRS data into the MongoDB collection.
+    result = srs_database[srs_main_collection].insert_one({"text": text})
+    srs_id = str(result.inserted_id)
+
+    response_body = {
+        "message": "text srs added successfully",
+        "srs_id": srs_id
+    }
+
+    return response_body
