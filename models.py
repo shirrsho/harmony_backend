c392cbd9ba9d92955f1efecd322ee0825e50569c
Shirsho-Lab <bsse1106@iit.du.ac.bd>
2023-07-27 17:54:19 +0600

    project creation  done

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 from fastapi import UploadFile
 
+class SRSProject(BaseModel):
+    project_title:str
+
 class SRSData(BaseModel):
     srs_title:str
     text: str
--- server.py
+++ server.py
@@ -6,15 +6,17 @@ from dotenv import load_dotenv
 from mongodb import MongoDB
 from bson.objectid import ObjectId
 
-from models import SRSData, SRSFile
+from models import SRSData, SRSFile, SRSProject
 load_dotenv()
 
 database_name = os.environ.get("DATABASE_NAME")
 srs_main_collection = os.environ.get("SRS_MAIN_COLLECTION")
 srs_ids_collection = os.environ.get("SRS_IDS_COLLECTION")
+srs_projects_collection = os.environ.get("SRS_PROJECTS_COLLECTION")
 
 srs_main_text_ep = os.environ.get("SRS_MAIN_TEXT_ENDPOINT")
 srs_main_file_ep = os.environ.get("SRS_MAIN_FILE_ENDPOINT")
+srs_project_ep = os.environ.get("SRS_PROJECTS_ENDPOINT")
 srs_ids_ep = os.environ.get("SRS_IDS_ENDPOINT")
 
 app = FastAPI()
@@ -32,6 +34,24 @@ app.add_middleware(
 )
 
 srs_database = MongoDB().get_client()[database_name]
+
+@app.post(srs_project_ep, status_code=201)
+async def add_project(data: SRSProject):
+    project_title = data.project_title
+    if not project_title:
+        raise HTTPException(status_code=400, detail="error in text srs adding", headers={"X-Error": "Text cannot be empty."})
+
+    # Insert the SRS data into the MongoDB collection.
+    result = srs_database[srs_projects_collection].insert_one({
+        "project_title":project_title
+    })
+
+    response_body = {
+        "message": "project added successfully",
+        "srs_id": str(result.inserted_id)
+    }
+
+    return response_body
 
 @app.post(srs_main_text_ep, status_code=201)
 async def add_srs_text(srs_data: SRSData):
@@ -102,16 +122,31 @@ async def get_srs_ids():
             'srs_id':str(srs_ids["_id"]),
             'srs_title':str(srs_ids["srs_title"])
             } for srs_ids in srs_ids_cursor]
-        
-        if srs_ids:
 
-            response_body = {
-                "message": "ids found and sent successfully",
-                "srs_ids": srs_ids
-            }
+        response_body = {
+            "message": "ids found and sent successfully",
+            "srs_ids": srs_ids
+        }
+        
+        return response_body
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="error in file srs sending", headers={"X-Error": str(e)})
+
+@app.get(srs_project_ep)
+async def get_projects():
+    try:
+        srs_projects_cursor = srs_database[srs_projects_collection].find()
+        srs_projects = [{
+            'project_id':str(srs_project["_id"]),
+            'project_title':str(srs_project["project_title"])
+            } for srs_project in srs_projects_cursor]
+
+        response_body = {
+            "message": "projects found and sent successfully",
+            "srs_projects": srs_projects
+        }
             
-            return response_body
-        else:
-            raise HTTPException(status_code=400, detail="error in file srs sending", headers={"X-Error": "SRS not found."})
+        return response_body
+        
     except Exception as e:
         raise HTTPException(status_code=400, detail="error in file srs sending", headers={"X-Error": str(e)})
